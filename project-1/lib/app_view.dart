8c9b2bf63a4f60eeddea92d36a5fab5efeba4343
RostikKalabishka <rostik31052002@gmail.com>
2024-02-18 19:25:33 +0200

    add example architecture

---
+++ project-1/lib/app_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
